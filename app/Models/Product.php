df11f3aa33ad151f30b7a26ae01a21b4125c7aa5
sasha <sashapozhidaev07@example.com>
2023-04-01 22:51:05 +0600

    filter

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,14 +6,12 @@ namespace App\Models;
 
 use App\Models\Category;
 use App\Models\Collection;
-use App\Models\Traits\Filterable;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Contracts\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Mehradsadeghi\FilterQueryString\FilterQueryString;
 
 class Product extends Model
 {
